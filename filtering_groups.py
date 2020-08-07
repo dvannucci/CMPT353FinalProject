a9d9b2dc062aa56cf2e05fb99ca3ce910f0ab2ae
bensonouyang <35547231+bensonouyang@users.noreply.github.com>
2020-08-07 00:44:55 -0700

    added k-means cluster

--- filtering_groups.py
+++ filtering_groups.py
@@ -5,7 +5,9 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import geopandas as gpd
+from sklearn.cluster import KMeans
 
 def main():
     data = pd.read_json('amenities-vancouver.json.gz', lines = True)
@@ -56,6 +58,9 @@ def main():
     family['type'] = 'family'
     pets['type'] = 'pets'
     safety['type'] = 'safety'
+    car['type'] = 'car'
+    public['type'] = 'public'
+    bike['type'] = 'bike'
 
     value1 = input("Enter first value: ")
     value2 = input("Enter second value: ")
@@ -77,7 +82,7 @@ def main():
 
     # conditions for reading inputs and selecting the dataframe for that specific input/value
 
-    allvals = pd.concat([nightlife,fitness,activities,leisure,family,pets,safety])
+    allvals = pd.concat([nightlife,fitness,activities,leisure,family,pets,safety,car,public,bike])
 
     for x in listofvalues:
         if value1 == x:
@@ -86,10 +91,14 @@ def main():
             seconddf = allvals[allvals['type'] == x]
         if value3 == x:
             thirddf = allvals[allvals['type'] == x]
+            
+    for y in listoftransport:
+        if transportation == y:
+            transportdf = allvals[allvals['type'] == y]
 
     # merged the 3 dataframes of the 3 values chosen by user
 
-    group = pd.concat([firstdf,seconddf,thirddf])
+    group = pd.concat([firstdf,seconddf,thirddf,transportdf])
 
     # filtered data to just Vancouver, not including Burnaby, Richmond, etc ..
     group = group[(group['lat']>= 49.2 )&(group['lat'] <= 49.3) & (group['lon'] >= -123.225 )&(group['lon'] <= -123.025)]
@@ -109,6 +118,33 @@ def main():
 
     ax.axis('off')
 
+    plt.show()
+    
+    # clustering https://datascience.stackexchange.com/questions/761/clustering-geo-location-coordinates-lat-long-pairs
+
+    arr = [[x,y] for x,y in zip(group['lon'],group['lat'])]
+    X = np.reshape(arr,(int(len(arr)),2))
+    model = KMeans(n_clusters=10)
+    y = model.fit_predict(X)
+
+    fig, ax = plt.subplots(1, figsize = (16,8))
+
+    bnd_gdf.plot(ax = ax, color = 'black')
+    ps_gdf.plot(ax = ax, color = 'white', alpha = 0.4)
+
+    # adapted from https://medium.com/pursuitnotes/k-means-clustering-model-in-6-steps-with-python-35b532cfa8ad
+
+    plt.scatter(X[y==0, 0], X[y == 0, 1], s=100, c='red', label ='Cluster 1')
+    plt.scatter(X[y==1, 0], X[y ==1, 1], s=100, c='blue', label ='Cluster 2')
+    plt.scatter(X[y==2, 0], X[y ==2, 1], s=100, c='green', label ='Cluster 3')
+    plt.scatter(X[y==3, 0], X[y ==3, 1], s=100, c='yellow', label ='Cluster 4')
+    plt.scatter(X[y==4, 0], X[y ==4, 1], s=100, c='orange', label ='Cluster 5')
+    plt.scatter(X[y==5, 0], X[y ==5, 1], s=100, c='cyan', label ='Cluster 6')
+    plt.scatter(X[y==6, 0], X[y ==6, 1], s=100, c='purple', label ='Cluster 7')
+    plt.scatter(X[y==7, 0], X[y ==7, 1], s=100, c='magenta', label ='Cluster 8')
+    plt.scatter(X[y==8, 0], X[y ==8, 1], s=100, c='pink', label ='Cluster 9')
+    plt.scatter(X[y==9, 0], X[y ==9, 1], s=100, c='crimson', label ='Cluster 10')
+    ax.axis('off')
     plt.show()
 
 if __name__=='__main__':
