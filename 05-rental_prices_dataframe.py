39d579d555b554f30fdd9a70921013b925c6751d
Daniel Vannucci <dano.vannucci@gmail.com>
2020-08-11 17:37:43 -0700

    pricing data added

--- 05-rental_prices_dataframe.py
+++ 05-rental_prices_dataframe.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
-import difflib as dl
 import geopandas as gpd
+from scipy import stats
 from shapely.geometry import Point
 
 # Data for most location prices from https://www.zumper.com/rent-research/vancouver-bc/kensington-cedar-cottage
 # https://vancouver.ca/files/cov/Arbutus-Ridge-census-data.pdf and https://vancouver.ca/files/cov/Shaughnessy-census-data.pdf sources for Shaughnessy and Arbutus-Ridge rental prices data.
-
 
 def locationFind(row):
     point = Point(row.thePoint[1], row.thePoint[0] )
@@ -18,11 +17,10 @@ def locationFind(row):
 
     return averageVancouverPrice
 
+
 bnd_gdf = gpd.read_file('boundary/local-area-boundary.shp')
 
 poly = bnd_gdf['geometry']
-
-new = blocksData = pd.read_json('VancouverBlocks.json.gz', lines=True)
 
 prices = pd.read_csv('rental_prices_data/rental_prices_manual.csv', thousands=',', index_col = 'Location')
 
@@ -30,6 +28,10 @@ prices.dropna(how='all', axis='columns', inplace=True)
 
 averageVancouverPrice = prices['Average Rent'].mean()
 
-new['rent_amount'] = new.apply(locationFind, axis = 1)
+priceDataframe = pd.read_json('VancouverBlocks.json.gz', lines=True)
 
-print(new)
+priceValues = priceDataframe.apply(locationFind, axis = 1)
+
+priceDataframe['sum_zscore'] = stats.zscore(priceValues)
+
+priceDataframe.to_json('Vancouver_blocks_zscores/price_zscores.json.gz', compression='gzip')
--- 07-driver-program.py
+++ 07-driver-program.py
@@ -45,11 +45,23 @@ def main():
         else:
             break
 
+    print("\nAnd last, when it comes to price, what describes you best,\n (1) Price is not an issue\n (2) I would prefer cheaper, but not the largest deal breaker\n (3) I am very price sensitive (i.e less expensive the better)")
+    sensitive = ['1','2','3']
+
+    while True:
+        priceweight = input("Enter 1, 2 or 3 based on the criteria above: ")
+        if (priceweight not in sensitive):
+            print("That is not either 1, 2 or 3.")
+            continue
+        else:
+            break
+
     # Read the appropriate dataframes.
     firstdf = pd.read_json('Vancouver_blocks_zscores/' + value1 + '_zscores.json.gz')
     seconddf = pd.read_json('Vancouver_blocks_zscores/' + value2 + '_zscores.json.gz')
     thirddf = pd.read_json('Vancouver_blocks_zscores/' + value3 + '_zscores.json.gz')
     transportdf = pd.read_json('Vancouver_blocks_zscores/' + transportation + '_zscores.json.gz')
+    pricedf = pd.read_json('Vancouver_blocks_zscores/price_zscores.json.gz')
 
     # Create a new dataframe with just the first five columns of the original dataframes. The columns location1, location2, location3, location4, and the point.
     newframe = firstdf.iloc[: , :5]
@@ -60,7 +72,7 @@ def main():
     # The new range that we are going to convert it to with a max value of 10 and a min of 1. This is an arbitrary range, the importance is that all the values will be greater than 0.
     new_range = 10 - 1
     # For each dataframe, make a new column with the converted sum_zscore normalized from 1 to 10.
-    for i, df in enumerate(['firstdf', 'seconddf', 'thirddf', 'transportdf'], start=1):
+    for i, df in enumerate(['firstdf', 'seconddf', 'thirddf', 'transportdf', 'pricedf'], start=1):
         df = vars()[df]
         old_range = max(df.sum_zscore) - min(df.sum_zscore)
         newframe['score' + str(i)] = (((df.sum_zscore - min(df.sum_zscore)) * new_range) / old_range) + 1
@@ -68,42 +80,44 @@ def main():
     # Calculation of the weighted score. The values in the exponents, 2, 1.4 and 1.2, were just guesses to be tested out.
     # https://pubsonline.informs.org/doi/pdf/10.1287/ited.2013.0124
     # Article for multiplicative scoring function with weight.
-    newframe['score'] = 1 / ( (newframe.score1 ** 2) * (newframe.score2 ** 1.6) * (newframe.score3 ** 1.2) * (newframe.score4 ** 1.4)  )
+    newframe['score'] = 1 / ( (newframe.score1 ** 2.5) * (newframe.score2 ** 2) * (newframe.score3 ** 1.5) * (newframe.score4 ** 2) * (newframe.score5 ** int(priceweight))  )
 
     # Find the index of the row with the max score
     index = newframe.score.idxmax()
 
     theRow = newframe.iloc[index]
 
+    print(theRow)
+
     print("\n\nYour ideal living location is within the addresses of,\n\n 1. %s \n 2. %s \n 3. %s \n 4. %s.\n\nThis location has the best overall %s, %s, and %s score in that order of importance, while taking into account your primary transportation mode, %s." %(theRow.location1, theRow.location2, theRow.location3, theRow.location4, value1, value2, value3, transportation))
-    
-    con = pd.concat([firstdf,seconddf,thirddf,transportdf])
+
+    con = pd.concat([firstdf,seconddf,thirddf,transportdf,pricedf])
 
     con = con[(con['lat']>= 49.2 )&(con['lat'] <= 49.3) & (con['lon'] >= -123.225 )&(con['lon'] <= -123.025)]
     #con = con.reset_index()
-    
+
     bnd_gdf = gpd.read_file('boundary/local-area-boundary.shp')
     ps_gdf = gpd.read_file('public-streets/public-streets.shp')
 
     poly = bnd_gdf['geometry']
-    
-    
+
+
     poly['coords'] = poly.apply(lambda x: x.representative_point().coords[:])
     poly['coords'] = [coords[0] for coords in poly['coords']]
-    
+
 
     fig, ax = plt.subplots(1, figsize = (30,12))
 
     bnd_gdf.plot(ax = ax, color = 'black')
     ps_gdf.plot(ax = ax, color = 'white', alpha = 0.4)
-    
+
 
     amen = con['amenity'].unique()
     for x in amen:
         agg = con.loc[con['amenity']==x]
         ax.scatter(agg['lon'],agg['lat'], label = x)
     ax.legend()
-    
+
     # https://automating-gis-processes.github.io/CSC18/lessons/L4/point-in-polygon.html
     point = Point(theRow.thePoint[1],theRow.thePoint[0])
     for i in range(len(poly) - 1):
@@ -116,16 +130,16 @@ def main():
         else:
             plt.plot(x, y, color='#6699cc', alpha=0.7,
             linewidth=3, solid_capstyle='round', zorder=2)
-        plt.annotate(s = bnd_gdf['name'][i], xy = poly['coords'][i], c = 'yellow', 
+        plt.annotate(s = bnd_gdf['name'][i], xy = poly['coords'][i], c = 'yellow',
                      horizontalalignment='center', fontsize = 15)
-        
-    
+
+
     plt.scatter(x = theRow.thePoint[1], y = theRow.thePoint[0], s = 1000,c = 'red',marker = (5,1))
-    
+
     plt.title('Amenities of\n Vancouver, BC', loc = 'left', fontsize = 40)
 
     ax.axis('off')
-    
+
     plt.show()
 
 if __name__ == '__main__':
